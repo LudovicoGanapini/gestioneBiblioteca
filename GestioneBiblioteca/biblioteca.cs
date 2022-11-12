fd089f1000e0c5c13b96628097189ec58fd37832
Ludovico Mario Ganapini <LUDOVICO.GANAPINI@local.ittcesena.it>
2022-11-12 08:57:31 +0100

    Versione 4, aggiunta classe biblioteca

---
+++ GestioneBiblioteca/biblioteca.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestioneBiblioteca
+{
+    internal class biblioteca
+    {
+        private string nome;
+        private string indirizzo;
+        private string oraApertura;
+        private string oraChiusura;
+        private string elencoLibri;
+
+
+    }
+}
